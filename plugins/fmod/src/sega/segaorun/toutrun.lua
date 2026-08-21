af531c42f76a27c83a2fb12db66934d431d6c202
opa <opa@opa.mydns.jp>
2026-08-22 08:30:38 +0900

    add outrun

---
+++ plugins/fmod/src/sega/segaorun/toutrun.lua
@@ -0,0 +1,19 @@
+local user = {}
+
+function user.init()
+	print("user script ok.")
+
+	function user.sound_replace(offset, data)
+		if data ~= 0x80 then
+			--MES("SOUND: %02X(%04X)", data, offset)
+			xvib:play(data)
+			if fmod:play(data) > 0 then data = 0x80 end
+			--mem:write_u16(0x2607B4, 0x0000)
+		end
+		return data
+	end
+
+	set_write_handlers(":soundcpu", 0xf800, user.sound_replace)
+end
+
+return user
